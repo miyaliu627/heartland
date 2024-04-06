90c5897b0d530054e1586d14b22504935d45b080
Elysia Lopez <elysialopez528@gmail.com>
2024-04-06 18:50:53 -0500

    Added MemoryOrb component

--- frontend/src/components/IslandPopup.jsx
+++ frontend/src/components/IslandPopup.jsx
@@ -1,8 +1,17 @@
+import "../output.css"
+import MemoryOrb from "./MemoryOrb";
+
 export default function IslandPopup()
 {
     return (
-        <div>
-            <img src="island.png" alt="island popup"></img>
+        <div class="relative h-screen flex items-center justify-center">
+            <img    src="https://i.pinimg.com/originals/7d/12/60/7d1260bff315dff2b41a0c2fc7d6fcd0.gif" 
+                    width="800" 
+                    alt="island popup"
+                    class="absolute">
+            </img>
+            <MemoryOrb x="10" y="20"/>
+            
         </div>
     );
 }
---
+++ frontend/src/components/MemoryOrb.jsx
@@ -0,0 +1,9 @@
+export default function MemoryOrb({x, y})
+{
+    return (
+        <img    src="https://cdn-icons-png.freepik.com/512/8239/8239169.png"
+                width="100px"
+                alt="map pin"
+                class="absolute bottom-0 left-0 transition duration-500 hover:scale-125 flex justify-center items-center"></img>
+    );
+}

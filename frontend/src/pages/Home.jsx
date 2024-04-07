a8369e29b9517775becbff5c6ddd9fc1f87fd67f
iris3ly <irisely2025@u.northwestern.edu>
2024-04-07 00:45:07 -0500

    did popups

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,13 @@
 import { useState, Suspense } from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
-
+import Popups from '../components/Popups';
 import Island from '../models/Island';
 import Sky from '../models/Sky';
 
-
 const Home = () => {
     const [isRotating, setIsRotating] = useState(false);
-    const [currentState, setCurrentStage] = useState(1);
+    const [currentStage, setCurrentStage] = useState(1);
 
     const adjustIslandForScreenSize = () => {
         let screenScale = null;
@@ -28,6 +27,9 @@ const Home = () => {
 
     return (
         <section className="w-full h-screen relative">
+            <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+                {currentStage && <Popups currentStage={currentStage} />}
+            </div>
             <Canvas
                 className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
                 camera={{ near: 0.1, far: 1000 }}
